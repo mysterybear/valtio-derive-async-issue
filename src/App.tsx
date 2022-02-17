9b6b11e68a0c710f9c2f11499f04157d79c5b3d0
Thomas O'Neill <tom@opensystemslab.io>
2022-02-17 18:09:02 +0000

    proxy with computed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React, { Suspense } from "react"
 import { proxy, useSnapshot } from "valtio"
-import { derive } from "valtio/utils"
+import { proxyWithComputed } from "valtio/utils"
 
 const asyncStore = proxy({
   users: fetch("https://reqres.in/api/users").then((res) => res.json()),
 })
 
-const derivedAsyncStore = derive({
-  doo: async (get) => {
-    return await get(asyncStore.users)
+// const derivedAsyncStore = derive({
+//   doo: async (get) => {
+//     return await get(asyncStore.users)
+//   },
+// })
+
+const derivedAsyncStore = proxyWithComputed(
+  {
+    users: asyncStore.users,
   },
-})
+  {
+    strung: (get) => JSON.stringify(get.users),
+  }
+)
 
 const AsyncStuff = () => {
-  const { doo } = useSnapshot(derivedAsyncStore)
+  const { users, strung } = useSnapshot(derivedAsyncStore)
   return (
     <div>
-      <pre>{JSON.stringify({ doo }, null, 2)}</pre>
+      <pre>{JSON.stringify({ users, strung }, null, 2)}</pre>
     </div>
   )
 }
