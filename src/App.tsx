4a5d5c746ea68d72130c9a5ee9337c5bf9dc2035
Thomas O'Neill <tom@opensystemslab.io>
2022-02-17 17:32:53 +0000

    derive async issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react"
+import { proxy, useSnapshot } from "valtio"
+import { derive } from "valtio/utils"
+
+const asyncStore = proxy({
+  users: fetch("https://reqres.in/api/users").then((res) => res.json()),
+})
+
+const derivedAsyncStore = derive({
+  doo: async (get) => {
+    return await get(asyncStore.users)
+  },
+})
+
+const AsyncStuff = () => {
+  const { doo } = useSnapshot(derivedAsyncStore)
+  return (
+    <div>
+      <pre>{JSON.stringify({ doo }, null, 2)}</pre>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={null}>
+        <AsyncStuff />
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
